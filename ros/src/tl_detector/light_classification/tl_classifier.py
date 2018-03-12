3010f71d6c94619f633d4f76c901c15b7c46fccc
Stephan Meyer <stm@autonomos.xyz>
2018-03-12 21:33:35 +0100

    add tl dataset for simulation; fix bridge timings and tl callback sync issues

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -9,35 +9,50 @@ import csv
 class TLClassifier(object):
 
     def __init__(self):
-        rospack = rospkg.RosPack()
-        self.path = os.path.join(rospack.get_path('tl_detector'), "sim_data")
-        if not os.path.exists(self.path):
-            os.makedirs(self.path)
-        f = open(os.path.join(self.path, 'labels.csv'), 'wb')
-        self.writer = csv.writer(f)
-        self.num_files = 0
-        self.last_light = None
+        self.probe_groundtruth_data = rospy.get_param("probe_groundtruth_data", False)
 
-    def get_classification(self, light, image):
+        if self.probe_groundtruth_data:
+            rospack = rospkg.RosPack()
+            self.path = os.path.join(rospack.get_path('tl_detector'), "sim_data")
+            if not os.path.exists(self.path):
+                os.makedirs(self.path)
+            labels_file = os.path.join(self.path, 'labels.csv')
+            if os.path.isfile(labels_file) :
+                raise RuntimeError("File %s already exists!", labels_file)
+            f = open(os.path.join(self.path, 'labels.csv'), 'wb')
+            self.writer = csv.writer(f)
+            self.num_files = 0
+            self.last_light_data = None
+            self.new_state = 0
+
+    def get_classification(self, light_data, image):
         """Determines the color of the traffic light in the image
 
         Args:
-            image (cv::Mat): image containing the traffic light
+            light_data (dict):  wrapped data about the TL
+            image (cv::Mat):    image containing the traffic light
 
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        cv2.imshow("input_image", image)
-        cv2.waitKey(1)
 
-        if not self.last_light or abs(self.last_light['distance'] - light['distance']) > 1:
-            filename = "tl_%05d.png" % self.num_files
-            cv2.imwrite(os.path.join(self.path, filename), image)
-            rospy.logdebug("Writing image to %s", filename)
-            self.writer.writerow([filename, light['light'].state])
-            self.last_light = light
-            self.num_files += 1
+        if self.probe_groundtruth_data:
+            #cv2.imshow("input_image", image)
+            #cv2.waitKey(1)
+
+            if not self.last_light_data or  abs(self.last_light_data['distance'] - light_data['distance']) > 1. or self.last_light_data['light'].state != light_data['light'].state:
+                # delivered ground-truth state from simulator and image are not in sync, so one has wait a little and delay the probe
+                if self.new_state >3:
+                    self.new_state = 0
+                    filename = "tl_%05d.png" % self.num_files
+                    cv2.imwrite(os.path.join(self.path, filename), image)
+                    rospy.logdebug("Writing image to %s", filename)
+                    self.writer.writerow([filename, light_data['light'].state])
+                    self.last_light_data = light_data
+                    self.num_files += 1
+                else: 
+                    self.new_state +=1
 
 
-        return light['light'].state
+        return light_data['light'].state
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 import rospy
+
+from light_classification.tl_classifier import TLClassifier
+
 from std_msgs.msg import Int32
 from geometry_msgs.msg import PoseStamped, Pose
 from styx_msgs.msg import TrafficLightArray, TrafficLight
 from styx_msgs.msg import Lane
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-from light_classification.tl_classifier import TLClassifier
-import tf
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+
 import cv2
 import numpy as np
 import yaml
@@ -22,8 +25,20 @@ class TLDetector(object):
         self.waypoints = None
         self.camera_image = None
         self.lights = []
-        self.use_ground_truth = rospy.get_param("~use_ground_truth", default=False)
+        self.state = TrafficLight.UNKNOWN
+        self.last_state = TrafficLight.UNKNOWN
+        self.last_wp = -1
+        self.state_count = 0
+        self.bridge = CvBridge()
+        self.light_classifier = TLClassifier()
+
+        self.use_ground_truth = rospy.get_param("~use_ground_truth", default=True)
         self.tl_consideration_distance = rospy.get_param("/tl_consideration_distance",  100)
+
+        config_string = rospy.get_param("/traffic_light_config")
+        self.config = yaml.load(config_string)
+
+        self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
         sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
@@ -35,23 +50,11 @@ class TLDetector(object):
         simulator. When testing on the vehicle, the color state will not be available. You'll need to
         rely on the position of the light and the camera image to predict it.
         '''
-        sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
-        sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
+        self.tss = ApproximateTimeSynchronizer([Subscriber("/vehicle/traffic_lights", TrafficLightArray),
+                                               Subscriber("/image_color", Image)], 30, 0.1)
+        self.tss.registerCallback(self.image_cb)
 
-        config_string = rospy.get_param("/traffic_light_config")
-        self.config = yaml.load(config_string)
-
-        self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
-
-        self.bridge = CvBridge()
-        self.light_classifier = TLClassifier()
-        self.listener = tf.TransformListener()
-
-        self.state = TrafficLight.UNKNOWN
-        self.last_state = TrafficLight.UNKNOWN
-        self.last_wp = -1
-        self.state_count = 0
-
+        
         rospy.spin()
 
     def pose_cb(self, msg):
@@ -61,7 +64,7 @@ class TLDetector(object):
         self.waypoints = waypoints
 
         """
-        # test get_waypoint_distance method
+        # test get_waypoint_distance method; TODO: move into rostest
         idx1 = 0
         idx2 = 5
         rospy.logerr("distance between %d and %d: %f", idx1, idx2, self.get_waypoint_distance(idx1, idx2))
@@ -87,19 +90,19 @@ class TLDetector(object):
         rospy.logerr("distance between %d and %d: %f", idx1, idx2, self.get_waypoint_distance(idx1, idx2))
         """
 
-    def traffic_cb(self, msg):
-        self.lights = msg.lights
-
-    def image_cb(self, msg):
+    def image_cb(self, traffic_lights_msg, image_msg):
         """Identifies red lights in the incoming camera image and publishes the index
             of the waypoint closest to the red light's stop line to /traffic_waypoint
 
         Args:
-            msg (Image): image from car-mounted camera
+            traffic_lights_msg (TrafficLightArray): image from car-mounted camera
+            image_msg (Image):                      image from car-mounted camera
 
         """
+        self.lights = traffic_lights_msg.lights
+        self.camera_image = image_msg
         self.has_image = True
-        self.camera_image = msg
+
         light_wp, state = self.process_traffic_lights()
 
         '''
@@ -121,6 +124,7 @@ class TLDetector(object):
         self.state_count += 1
 
     # TODO: move into common package (might be cool to create a map-class: map = Map(waypoints), map.get_waypoint_distance(..))
+    # TODO: in simulation, this takes a non-trivial amount of resources and slowes down the whole pipeline
     def get_closest_waypoint_idx(self, pose):
         """Identifies the closest path waypoint to the given position
             https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
@@ -134,10 +138,12 @@ class TLDetector(object):
         closest_idx = 0
         closest_dist = float("inf")
         for idx, wp in enumerate(self.waypoints.waypoints):
-            curr_dist = np.sqrt((wp.pose.pose.position.x - pose.position.x)**2 + (wp.pose.pose.position.y - pose.position.y)**2)
-            if curr_dist < closest_dist:
+            dx = wp.pose.pose.position.x - pose.position.x
+            dy = wp.pose.pose.position.y - pose.position.y
+            dist = dx*dx + dy*dy
+            if dist < closest_dist:
                 closest_idx = idx
-                closest_dist = curr_dist
+                closest_dist = dist
 
         return closest_idx
 
@@ -176,12 +182,12 @@ class TLDetector(object):
         idx = wp_idx1
         while idx != wp_idx2:
             p1 = self.waypoints.waypoints[idx].pose.pose.position
-            p2 = self.waypoints.waypoints[idx+1].pose.pose.position
+            p2 = self.waypoints.waypoints[(idx+1) % len(self.waypoints.waypoints)].pose.pose.position
             dist = np.sqrt((p2.x - p1.x)**2 + (p2.y - p1.y)**2)
             distance += dist
             
             idx += 1
-            if idx + 1 >= len(self.waypoints.waypoints):
+            if idx >= len(self.waypoints.waypoints):
                 idx = 0
 
         return distance * dir
@@ -258,7 +264,7 @@ class TLDetector(object):
         if not len(relevant_lights):
             rospy.logdebug("No relevant TL found")
             return -1, TrafficLight.UNKNOWN
-        
+
 
         # sort by distance and select the closes one
         relevant_lights.sort(key=lambda x: x['distance'])
