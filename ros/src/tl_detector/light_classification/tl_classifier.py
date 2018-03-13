daac2ade09d9d41523b5712f04a214d7542f96ec
ashok <wuffusg@gmail.com>
2018-03-13 00:02:43 -0700

    Add code to conditionally save training data, or load trained model and use it to predict traffic light state.
    Note that currently the simulator is a few frames behind the state that it
    provides so that on traffic light state transitions we still need to manually
    correct the labels.csv file.
    
    Add train.py to train a keras model and save it so that it can be used
    for traffic light prediction.
    
    Add test_model.py so we can run a quick sanity check on the model.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -4,19 +4,33 @@ import cv2
 import rospkg
 import os
 import csv
+import keras
+import numpy as np
+import tensorflow as tf
 
 
 class TLClassifier(object):
 
     def __init__(self):
-        rospack = rospkg.RosPack()
-        self.path = os.path.join(rospack.get_path('tl_detector'), "sim_data")
-        if not os.path.exists(self.path):
-            os.makedirs(self.path)
-        f = open(os.path.join(self.path, 'labels.csv'), 'wb')
-        self.writer = csv.writer(f)
-        self.num_files = 0
         self.last_light = None
+
+        # TODO need to be more robust, perhaps use a ros parameter instead
+        self.gen_train_data = True
+        
+        if self.gen_train_data:
+            self.num_files = 0
+            rospack = rospkg.RosPack()
+            self.path = os.path.join(rospack.get_path('tl_detector'), "sim_data")
+            if not os.path.exists(self.path):
+                os.makedirs(self.path)
+            f = open(os.path.join(self.path, 'labels.csv'), 'wb')
+            self.writer = csv.writer(f)
+        else:
+            # TODO need to add check that the model is compatible with current keras version
+            self.model = keras.models.load_model("model.h5")
+            self.model._make_predict_function()
+            rospy.loginfo("keras model loaded")
+            self.graph = tf.get_default_graph()
 
     def get_classification(self, light, image):
         """Determines the color of the traffic light in the image
@@ -28,16 +42,34 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        cv2.imshow("input_image", image)
-        cv2.waitKey(1)
+        # read and save the light state immediately because it can change
+        # by the time we save the image
+        lstate = light['light'].state
 
-        if not self.last_light or abs(self.last_light['distance'] - light['distance']) > 1:
-            filename = "tl_%05d.png" % self.num_files
-            cv2.imwrite(os.path.join(self.path, filename), image)
-            rospy.logdebug("Writing image to %s", filename)
-            self.writer.writerow([filename, light['light'].state])
-            self.last_light = light
-            self.num_files += 1
+        # Reduce image to to 1/4 size
+        # rs_image = cv2.resize(image, None, fx=0.5, fy=0.5)
+        
 
+        if self.gen_train_data:
+            # cv2.imshow("input_image", image)
+            # cv2.waitKey(1)
 
-        return light['light'].state
+            if not self.last_light or abs(self.last_light['distance'] - light['distance']) > 1:
+                filename = "tl_%05d.png" % self.num_files
+                cv2.imwrite(os.path.join(self.path, filename), image)
+                rospy.logdebug("Writing image to %s", filename)
+                self.writer.writerow([filename, lstate])
+                self.last_light = light
+                self.num_files += 1
+
+            return lstate
+
+        else:
+            image_array = np.asarray(image)
+            # rospy.loginfo("Calling model prediction image shape %s", np.shape(image_array))
+            with self.graph.as_default():
+                light_predict = self.model.predict(image_array[None,:,:,:], batch_size=1)
+            # light_predict = self.model.predict(image_array[None,:,:,:], batch_size=1)
+            lightval = np.argmax(light_predict)
+            rospy.loginfo("Returned from model prediction %s, pred: %d, state: %d", light_predict, lightval, light['light'].state)
+            return lightval
